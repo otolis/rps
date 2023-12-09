612dc7350b92c0d83fef29143b59c0e6b76d9eb7
otolis <lagonikasa@gmail.com>
2023-12-09 17:36:55 +0200

    finished javascript code and it outputs to consolelog

--- rps.js
+++ rps.js
@@ -1,14 +1,44 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-        return "rock";
+            return "rock";
         case 1:
-        return "paper";
+            return "paper";
         case 2:
-        return "scissors";
-
+            return "scissors";
     }
 }
+
 let computerChoice = getComputerChoice();
-console.log(computerChoice);
+let playerChoice = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+console.log("Computer choice:", computerChoice);
+console.log("Player choice:", playerChoice);
+
+function playRound(playerChoice, computerChoice) {
+    let result = "";
+    if (
+        (computerChoice === "rock" && playerChoice === "scissors") ||
+        (computerChoice === "paper" && playerChoice === "rock") ||
+        (computerChoice === "scissors" && playerChoice === "paper")
+    ) {
+        result = "computer wins";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "player wins";
+    } else {
+        result = "tie";
+    }
+    return result;
+}
+
+console.log(playRound(playerChoice, computerChoice));
+let i=0;
+function game () {
+    for (leti=0;i<=5;i++) {
+        
+    }
+}
